3366f40038c361ab0243b531fbcb9abc1c89faeb
Evan Eckels <eeckels2@illinois.edu>
2020-05-14 18:12:33 -0500

    try to fix transition stuff

--- js/dark-mode-cont.js
+++ js/dark-mode-cont.js
@@ -3,3 +3,5 @@ var stylesheet = document.getElementById('dark-stylesheet').rel;
 if (stylesheet === 'stylesheet') {
     document.getElementById('darkmode-slider').checked = true;
 }
+
+document.body.style.transition = "background-color 0.25s, color 0.15s";
